0b0e32b70ddca6890b61a79710ef0d15a934f2fc
wuyexkx <1191468572@qq.com>
2020-06-08 09:38:55 +0800

    位域 和 对齐方式

--- bit_field_memAlign.c
+++ bit_field_memAlign.c
@@ -1,3 +1,13 @@
+#if 0
+filename="$0"
+name="${filename%.*}"
+CC="gcc"
+$CC -o $name $filename
+$name $@
+rm $name
+exit
+#endif
+
 #include<stdio.h>
 
 // 位域 C语言struct 和 union特有的bit操作
@@ -11,14 +21,14 @@ struct bitField2 {
 };
 
 // 内存对齐 内存是按字节存放，但是取指令是按4 8等连续字节，提高效率，避免多次取值合并造成的效率问题
-// win下gcc中默认#pragma pack(8)，可以通过预编译命令#pragma pack(n)，n = 1,2,4,8,16来改变这一系数。
-#pragma pack(4) // alignment must be a small power of two
+// win下gcc中默认#pragma pack(8) linux也是，可以通过预编译命令#pragma pack(n)，n = 1,2,4,8,16来改变这一系数。
+#pragma pack(8) // alignment must be a small power of two
 
 // 有效对齐值：是给定值#pragma pack(n)和结构体中最长数据类型长度中较小的那个。有效对齐值也叫对齐单位。
     // 有效对齐值 = min(n, max size member of struct)
 
 // struct的大小需要满足下面两个条件 一个约束成员，一个约束总大小：    
-// (1) 结构体第一个成员的偏移量（offset）为0，offset = 整数倍的min(有效对齐值, max size member of struct)
+// (1) 结构体第一个成员的偏移量（offset）为0，offset = 整数倍的min(有效对齐值, size of this member)
     // 以后每个成员相对于结构体首地址的 offset 都是该成员大小与有效对齐值中较小那个的整数倍
     // 如有需要编译器会在成员之间加上填充字节。
 // (2) 结构体的总大小为 有效对齐值 的整数倍，如有需要编译器会在最末一个成员之后加上填充字节。
