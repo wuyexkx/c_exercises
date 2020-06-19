a750f3045e77966f2797ef4079dd40c01af07c04
wuyexkx <1191468572@qq.com>
2020-06-19 21:58:29 +0800

    纯虚函数的类叫抽象类 不能生成对象。基类指针访问虚函数根据子类继承来访问（多态）

---
+++ nowcoder/封装继承多态.c
@@ -0,0 +1,28 @@
+#if 0
+filename="$0"
+name="${filename%.*}"
+CC="gcc"
+$CC -o name filename 
+./name &@
+rm name
+exit
+#endif
+
+
+// 有纯虚函数的类叫做抽象类(abstract class)，它不能生成对象。
+
+// 用基类指针访问虚函数:
+//  这时虽然大家都是Animal*，但是调用的Speak函数却是各自的类实现的Speak。
+//  用哪个子类初始化基类指针就可以调用对应子类实现的虚函数。
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    char 
+    sscanf("A,0.444,333333,3.1415926",);
+    printf("=========\n");
+    return 0;
+}
